6c363a78eeec239cc7f852749c78e0b75b8cff89
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-05-26 16:43:49 +0200

    upd

--- celery/config.py
+++ celery/config.py
@@ -1,9 +1,9 @@
 import os
 
 AMQP = {
-    "username": os.environ.get("RABBITMQ_USER"),
-    "password": os.environ.get("RABBITMQ_PASSWORD"),
-    "host": os.environ.get("RABBITMQ_HOST", "rabbitmq"),
+    "username": os.environ.get("RABBITMQ_USER", "rabbitmq_usr"),
+    "password": os.environ.get("RABBITMQ_PASSWORD", "rabbitmq_password"),
+    "host": os.environ.get("RABBITMQ_HOST", "localhost"),
     "port": os.environ.get("RABBITMQ_PORT", "5672"),
     "vhost": os.environ.get("RABBITMQ_VHOST", "/"),
 }
