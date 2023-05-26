cd5d360a76035b5d378d8548af1330548464bf46
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-05-26 23:27:33 +0200

    authorization added

--- engine/server/app.py
+++ engine/server/app.py
@@ -1,8 +1,19 @@
-from apiflask import APIFlask
+from apiflask import APIFlask, HTTPTokenAuth
 from . import tasks
 from .schemas import TextToTranslate
+from .config import API_TOKEN
 
 app = APIFlask(__name__)
+
+auth = HTTPTokenAuth(scheme="ApiKey", header="X-API-Key")
+
+
+@auth.verify_token
+def verify_token(token):
+    print(token)
+    if token == API_TOKEN:
+        return token
+    print("Invalid token")
 
 
 @app.get("/")
@@ -11,6 +22,7 @@ def say_hello():
 
 
 @app.post("/translate")
+@app.auth_required(auth)
 @app.input(TextToTranslate)
 def translate(data):
     return {"taskId": tasks.translate.delay(data).id}
