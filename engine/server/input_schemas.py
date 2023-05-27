5c0d8e25e772a80517c40786de2f2240dd3c0ae1
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-05-28 00:01:01 +0200

    style changed

--- engine/server/input_schemas.py
+++ engine/server/input_schemas.py
@@ -5,8 +5,8 @@ from apiflask.validators import Length
 
 
 class TextToTranslate(Schema):
-    originalLanguage = String(required=True, validate=Length(0, 256))
-    targetLanguage = String(required=True, validate=Length(0, 256))
-    textType = String(required=True, validate=Length(0, 256))
+    original_language = String(required=True, validate=Length(0, 256))
+    target_language = String(required=True, validate=Length(0, 256))
+    text_type = String(required=True, validate=Length(0, 256))
     text = String(required=True, validate=Length(0, 5000))
     hook = String(required=False, validate=Length(0, 256))
